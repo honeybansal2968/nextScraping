64e413a9a36ae36049cb3d47b19aeefd6c3dbf8f
honeybansal66 <honeybansalcloud9@gmail.com>
2024-12-01 10:57:39 +0530

    af

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8090;
 
-app.use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname, 'public')))
+//     .set('views', path.join(__dirname, 'views'))
+//     .set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
